c981a80fa027c6e5e52af5e02e40e2281bdb6b0a
Victor Gomes <victor.gomes63@etec.sp.gov.br>
2021-08-29 15:09:30 -0300

    feat: add user data validation

--- api/src/controllers/UserController.js
+++ api/src/controllers/UserController.js
@@ -1,18 +1,68 @@
 const User = require("../models/User");
 const bcrypt = require('bcryptjs');
+const utils = require('../utils');
 
 class UserController{
     async create(req, res){
         const {userName, email, password} = req.body;
+        
+        if(userName == undefined || utils.isEmpty(userName) || !utils.isValidLength(userName, 3)){
+            res.statusCode = 406;
+            res.json({
+                status: false,
+                msg: "O nome de usuário deve ter pelo menos 3 caracteres"
+            });
+            return;
+        }else if(utils.isSpaced(userName)){
+            res.statusCode = 406;
+            res.json({
+                status: false,
+                msg: "O nome de usuário não deve ter espaços"
+            });
+            return;
+        }
+
+        if(email == undefined || utils.isEmpty(email) || !utils.isValidLength(email, 2)){
+            res.statusCode = 406;
+            res.json({
+                status: false,
+                msg: "O email deve ter mais de 2 caracteres"
+            });
+            return;
+        }else if(utils.isSpaced(email)){
+            res.statusCode = 406;
+            res.json({
+                status: false,
+                msg: "O email não deve ter espaços"
+            });
+            return;
+        }else if(!utils.isValidEmail(email)){
+            res.statusCode = 406;
+            res.json({
+                status: false,
+                msg: "Digite um email válido"
+            });
+            return;
+        }
+
+        if(!utils.isValidLength(password, 8)){
+            res.statusCode = 406;
+            res.json({
+                status: false,
+                msg: "A senha deve ter pelo menos 8 caracteres"
+            });
+            return;
+        }
 
         const salt = bcrypt.genSaltSync(10);
         const hash = bcrypt.hashSync(password, salt);
-
+        
         const data = {
             userName,
             email,
             password: hash
         }
+
 
         const { status, user } = await User.findByEmail(data.email);
         if(user.length == 0){
