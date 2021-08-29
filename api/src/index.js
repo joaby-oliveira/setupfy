31a9726e8b833f8fe7879750d0533682a6448c5e
Victor Gomes <victor.gomes63@etec.sp.gov.br>
2021-08-29 16:03:20 -0300

    fix: change back-end port

--- api/src/index.js
+++ api/src/index.js
@@ -8,6 +8,6 @@ app.use(bodyParser.json());
 
 app.use(Router);
 
-app.listen(3000, ()=>{
-    console.log('Server is running on http://localhost:3000');
+app.listen(8080, ()=>{
+    console.log('Server is running on http://localhost:8080');
 });
