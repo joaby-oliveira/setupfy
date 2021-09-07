3e21196d1f6541414b1d9fa508f43649a8d851a4
Victor Gomes <victor.gomes63@etec.sp.gov.br>
2021-09-07 13:09:20 -0300

    feat: add post create method 2/2

--- api/src/controllers/PostController.js
+++ api/src/controllers/PostController.js
@@ -1,23 +1,42 @@
 const Post = require('../models/Post');
+const Tag = require("../models/Tag");
 
 class PostController{
     async create(req, res){
         try{
-            const {content, user_id, tags} = req.body;
+            const {description, user_id, tags = []} = req.body;
             const data = {
-                content,
+                description,
                 likes: 0,
                 user_id
             }
             
             const post = await Post.create(data);
 
+            tags.map(async (tag) => {
+                const tagExists = await Tag.findOne(tag);
+                let tagId = 0;
+                if(tagExists.length != 0){
+                    tagId = tagExists[0].id;
+                }else{
+                    const tagData ={
+                        tag
+                    }
+                    tagId = await Tag.create(tagData);
+                }
+                const tag_postData = {
+                    post_id: post,
+                    tag_id: tagId
+                }
+                return await Post.tag_postInsert(tag_postData);
+            });
+
             res.statusCode = 201;
-            res.json({status: true, msg: "Post inserido!"})
+            res.json({status: true, msg: "Post inserido!"});
 
         }catch(err){
             res.statusCode = 406;
-            res.json({status: false, msg: "Post não pode ser inserido " + err})
+            res.json({status: false, msg: "Post não pode ser inserido " + err});
         }
     }
 }
--- api/src/models/Post.js
+++ api/src/models/Post.js
@@ -5,6 +5,11 @@ class Post {
         const post = await database.insert(data).table("posts");
         return post[0]
     }
+    
+    async tag_postInsert(data){
+        const post = await database.insert(data).table("tag_post");
+        return post[0]
+    }
 }
 
 module.exports = new Post();
