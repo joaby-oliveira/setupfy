f576acebb445119f06e977c74fd122bf2b9a0543
Victor Gomes <victor.gomes63@etec.sp.gov.br>
2021-09-10 21:54:12 -0300

    fix: solve some problems on user model and controller

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -2,88 +2,63 @@ const database = require("../database/database");
 
 class User{
     async create(data){
-        try{
-            const user = await database.insert(data).table("users");
-            return {status: true, msg: 'Usuário inserido', user}
-        }catch(err){
-            return {status: false, msg: 'Usuário não pode ser inserido'}
-        }
+        const user = await database.insert(data).table("users");
+        return user
     }
 
     async findByEmail(email){
-        try{
-            let user = await database.select().table("users").where({email: email});
-            return {status: true, user}
-        }catch(err){
-            return {status: false, msg: err}
-        }
+        let user = await database.select().table("users").where({email: email});
+        return {user}
     }
 
     async findByName(userName){
-        try{
-            let users = await database.select(["users.id", "users.userName", "users.email", "userImages.url as imgUrl"])
+        let users = await database.select(["users.id", "users.userName", "users.email", "userImages.url as imgUrl"])
             .table("users")
             .innerJoin("userImages", "userImages.user_id", "users.id")
             .where('userName', 'like', '%'+ userName +'%');
 
-            return {status: true, users}
-        }catch(err){
-            return {status: false, err}
-        }
+        return {users}
     }
 
     async findAllUsers(){
-        try{
-            let user = await database.select(["users.id", "users.userName", "users.email", "userImages.url as imgUrl"])
+        let user = await database.select(["users.id", "users.userName", "users.email", "userImages.url as imgUrl"])
             .table("users")
-            .innerJoin("userImages", "userImages.user_id", "users.id")
-            return {status: true, user}
-        }catch(err){
-            return {status: false, user}
-        }
+            .leftJoin("userImages", "userImages.user_id", "users.id")
+        return {user}
     }
 
     async findById(id){
-            let users = await database.select(["users.id", "users.userName", "users.email", "users.password", "userImages.url as imgUrl"])
+        let users = await database.select(["users.id", "users.userName", "users.email", "users.password", "userImages.url as imgUrl"])
             .table("users")
-            .innerJoin("userImages", "userImages.user_id", "users.id")
-            .where("user_id", id);
+            .leftJoin("userImages", "userImages.user_id", "users.id")
+            .where("users.id", id);
 
-            return {users};
+        return {users};
     }
 
     async update(data, id){
-        try{
-           let result = await database.update(data).table("users").where({id: id})
-           return {status: true, msg: "Usuário atualizado com sucesso", result}
-        }catch(err){
-            return {status: false, msg: 'Usuário não pode ser atualizado erro: ' + err}
-        }
+        let user = await database.update(data).table("users").where({id: id})
+        return {user}
     }
+
     async insertImage(image){
-        try{
-            await database.insert(image).table("userImages");
-            return {status: true, msg: 'Imagem inserida'}
-        }catch(err){
-            return {status: false, err: err}
-        }
+        let img = await database.insert(image).table("userImages");
+        return { img }
     }
+
     async updateImage(image, user_id){
-        try{
-            await database.update(image).table("userImages").where({user_id: user_id});
-            return {status: true, msg: 'Imagem atualizada'}
-        }catch(err){
-            return {status: false, err: err}
-        }
+        let img = await database.update(image).table("userImages").where({user_id: user_id});
+        return { img }
     }
 
     async findImage(id){
-        try{
-            let image = await database.select().table("userImages").where({user_id: id});
-            return {status: true, image}
-        }catch(err){
-            return {status: false, err}
-        }
+        let image = await database.select().table("userImages").where({user_id: id});
+        return {image}
+    }
+
+    async deleteImage(id){
+        let image = await database.delete().table("userImages").where({user_id: id});
+        return {image}
     }
 }
 
