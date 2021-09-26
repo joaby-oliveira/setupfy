b3714f123997b485398a316af1ebd49aad309b5f
Joaby Oliveira <joabyoliveira2004@gmail.com>
2021-09-26 13:21:16 -0300

    feat: add setupfy logo to login form

--- setupfy/src/components/LoginForm.js
+++ setupfy/src/components/LoginForm.js
@@ -2,13 +2,15 @@ import styles from '../../styles/components/Forms.module.scss'
 import { Input } from './Input'
 import { Button } from './Button'
 import { useForm } from '../Hooks/useForm'
+import Logo from '../../public/setupfy.svg'
 
 export const LoginForm = () => {
   const username = useForm('username')
   const password = useForm('password')
 
   return (
-    <form className={styles.form}>
+    <form className={`${styles.form} flex column crossCenter`}>
+      <Logo className={styles.logo}/>
       <Input label="Nome de usuário" name="username" {...username}/>
       <Input label="Senha" name="password" {...password}/>
       <Button>Criar conta</Button>
