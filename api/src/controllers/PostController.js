c88151133e9cd04c7b08d40a799c62881e6354d1
Victor Gomes <victor.gomes63@etec.sp.gov.br>
2021-09-28 14:35:04 -0300

    feat: add post validations

--- api/src/controllers/PostController.js
+++ api/src/controllers/PostController.js
@@ -1,60 +1,100 @@
 const Post = require('../models/Post');
 const Tag = require("../models/Tag");
 const User = require("../models/User");
+const utils = require("../utils")
 const path = require('path');
 const { unlink } = require('fs');
 const imgPath = path.resolve(__dirname, '..', '..', 'tmp', 'images')
+
+const postValidation = {
+    string: (string, res, value, length) => {
+        if (string === undefined || utils.isEmpty(string) || !utils.isValidLength(string, length)) {
+            res.statusCode = 406
+            res.json({
+              status: false,
+              msg: 'O campo ' + value + ' deve ter pelo menos ' + length + ' caracteres. '
+            })
+            res.utilized = true
+            return false
+        }
+        if (utils.isDoubleSpaced(string) && res.utilized === false) {
+          res.statusCode = 406
+          res.json({
+            status: false,
+            msg: 'O campo ' + value + ' não deve ter mais de um espaço entre as palavras'
+          })
+          res.utilized = true
+          return false
+        }
+    },
+    number: (price, res, length, value) => {
+        if (!utils.isValidMaxLength(price, length)) {
+          res.statusCode = 406
+          res.json({
+            status: false,
+            msg: 'O ' + value + ' deve ter no máximo ' + length + ' caracteres'
+          })
+          res.utilized = true
+        }
+      }
+}
 
 class PostController{
     async create(req, res){
         try{
             const {description, user_id, tags = []} = req.body;
-            const {users: user} = await User.findById(user_id)
-            if (user.length > 0) {
-                const data = {
-                    description,
-                    likes: 0,
-                    user_id
-                }
-                
-                const post = await Post.create(data);            
+            res.utilized = false
 
-                tags.map(async (tag) => {
-                    const tagExists = await Tag.findOne(tag);
-                    let tagId = 0;
-                    if(tagExists.length != 0){
-                        tagId = tagExists[0].id;
-                    }else{
-                        const tagData ={
-                            tag
+            postValidation.string(description, res, "Descrição", 2)
+
+            if (res.utilized == false) {
+                const {users: user} = await User.findById(user_id)
+                if (user.length > 0) {
+                    const data = {
+                        description,
+                        likes: 0,
+                        user_id
+                    }
+                    
+                    const post = await Post.create(data);            
+
+                    tags.map(async (tag) => {
+                        const tagExists = await Tag.findOne(tag);
+                        let tagId = 0;
+                        if(tagExists.length != 0){
+                            tagId = tagExists[0].id;
+                        }else{
+                            const tagData ={
+                                tag
+                            }
+                            tagId = await Tag.create(tagData);
                         }
-                        tagId = await Tag.create(tagData);
-                    }
-                    const tag_postData = {
-                        post_id: post,
-                        tag_id: tagId
-                    }
-                    return await Post.tag_postInsert(tag_postData);
-                });
+                        const tag_postData = {
+                            post_id: post,
+                            tag_id: tagId
+                        }
+                        return await Post.tag_postInsert(tag_postData);
+                    });
 
-                if (req.file != undefined) {
-                    const image = {
-                        name: req.file.filename,
-                        url: process.env.FILES_URL + req.file.filename,
-                        post_id: post
+                    if (req.file != undefined) {
+                        const image = {
+                            name: req.file.filename,
+                            url: process.env.FILES_URL + req.file.filename,
+                            post_id: post
+                        }
+                        await Post.insertImage(image);
                     }
-                    await Post.insertImage(image);
+
+                    res.statusCode = 201;
+                    res.json({status: true, msg: "Post inserido!"});
+                } else {
+                    if(req.file != undefined)
+                        unlink(imgPath + '/' + req.file.filename, (err) => {
+                            if (err) throw err
+                        })
+                    res.statusCode = 404;
+                    res.json({status: false, msg: "Usuário não encontrado na base de dados"});
                 }
-
-                res.statusCode = 201;
-                res.json({status: true, msg: "Post inserido!"});
-            } else {
-                if(req.file != undefined)
-                    unlink(imgPath + '/' + req.file.filename, (err) => {
-                        if (err) throw err
-                    })
-                res.statusCode = 404;
-                res.json({status: false, msg: "Usuário não encontrado na base de dados"});
             }
         }catch(err){
             if(req.file != undefined)
@@ -194,61 +234,67 @@ class PostController{
         try {
             const {id} = req.params;
             const {description, tags = []} = req.body;
-            const post = await Post.findById(id)
-            if (post.length > 0) {
-                const data = {
-                    id: post[0].id,
-                    description: post[0].description,
-                    likes: post[0].likes,
-                    user_id: post[0].user_id
-                }
+            res.utilized = false
 
-                if (description != undefined) {
-                    data.description = description
-                }
+            postValidation.string(description, res, "Descrição", 2)
 
-                await Post.update(data, id)
-                if ( tags.length > 0) {
-                    await Post.tag_postDelete(id);
-                    for (const tag of tags) {
-                        const tagExists = await Tag.findOne(tag);
-                        let tagId = 0;
-                        if(tagExists.length != 0){
-                            tagId = tagExists[0].id;
-                        }else{
-                            const tagData = { tag }
-                            tagId = await Tag.create(tagData);
+            if (res.utilized == false) {
+                const post = await Post.findById(id)
+                if (post.length > 0) {
+                    const data = {
+                        id: post[0].id,
+                        description: post[0].description,
+                        likes: post[0].likes,
+                        user_id: post[0].user_id
+                    }
+
+                    if (description != undefined) {
+                        data.description = description
+                    }
+
+                    await Post.update(data, id)
+                    if ( tags.length > 0) {
+                        await Post.tag_postDelete(id);
+                        for (const tag of tags) {
+                            const tagExists = await Tag.findOne(tag);
+                            let tagId = 0;
+                            if(tagExists.length != 0){
+                                tagId = tagExists[0].id;
+                            }else{
+                                const tagData = { tag }
+                                tagId = await Tag.create(tagData);
+                            }
+                            const tag_postData = {
+                                post_id: post[0].id,
+                                tag_id: tagId
+                            }
+                            await Post.tag_postInsert(tag_postData);
                         }
-                        const tag_postData = {
-                            post_id: post[0].id,
-                            tag_id: tagId
+                    }
+
+                    if(req.file != undefined) {
+                        const {img} = await Post.findImage(id);
+                        
+                        const image = {
+                            name: req.file.filename,
+                            url: process.env.FILES_URL + req.file.filename,
+                            post_id: id
                         }
-                        await Post.tag_postInsert(tag_postData);
+
+                        if (img.length > 0) { 
+                            unlink(imgPath + '/' + img[0].name, (err) => {})
+                            await Post.updateImage(image, id);
+                        } else {
+                            await Post.insertImage(image)
+                        }
                     }
+
+                    res.statusCode = 200;
+                    res.json({status: true, msg: "Post atualizado com sucesso"});
+                } else {
+                    res.statusCode = 404;
+                    res.json({status: false, msg: "Post não foi encontrado"});
                 }
-
-                if(req.file != undefined) {
-                    const {img} = await Post.findImage(id);
-                    
-                    const image = {
-                        name: req.file.filename,
-                        url: process.env.FILES_URL + req.file.filename,
-                        post_id: id
-                    }
-
-                    if (img.length > 0) { 
-                        unlink(imgPath + '/' + img[0].name, (err) => {})
-                        await Post.updateImage(image, id);
-                    } else {
-                        await Post.insertImage(image)
-                    }
-                }
-
-                res.statusCode = 200;
-                res.json({status: true, msg: "Post atualizado com sucesso"});
-            } else {
-                res.statusCode = 404;
-                res.json({status: false, msg: "Post não foi encontrado"});
             }
         } catch (err) {
             res.statusCode = 406;
